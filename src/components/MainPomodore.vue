ab94899819b78a24b9c39252dc4e965717a5b643
Dante Mogni <dantejoaquin.mogni@gmail.com>
2021-06-24 20:56:21 -0300

    Working Pomodoro

--- src/components/MainPomodore.vue
+++ src/components/MainPomodore.vue
@@ -1,18 +1,50 @@
 <template>
-    <div class="main shadow">
+    <transition appear name="bounce">
+        <div class="main shadow" 
+            :class="{resume: this.isTimerPaused&&!this.isTimerReset, 
+            pause: !this.isTimerPaused&&!this.isTimerReset}">
         <!-- 
             Al apretar "Start", se eliminan los atributos 'disabled'
             Cuando se aprieta "Start", se cambia el nombre del boton a Pausa.    
          -->
         <div>
             <span class="p-title">POMODORO TIMER</span>
-            <span class="p-clock">25:00</span>
+            <div class="p-clock">
+                <span>{{minutes}}</span>
+                <span>:</span>
+                <span>{{seconds}}</span>
+            </div>
         </div>
        <!-- Fijarse otra forma de camviar de Start/Pause usando funciones o un watch --> 
-        <button @click="this.isToggle = !this.isToggle" type="button" class="btn btn-primary item-main start-p"><span v-if="!this.isToggle">Start</span><span v-if="this.isToggle">Pause</span></button>
-        <button type="button" class="btn btn-outline-secondary item-main add-five" disabled>Add 5'</button>
-        <button type="button" class="btn btn-outline-danger item-main stop-p" disabled>Stop</button>
-    </div>
+       <transition name="fade" mode="out-in">
+            <button 
+                v-if="this.isTimerReset" 
+                @click="handleStart" 
+                type="button" 
+                class="btn btn-primary item-main start-p">Start</button>
+            <button 
+                v-else-if="!this.isTimerReset && !this.isTimerPaused" 
+                @click="handlePause" 
+                type="button" 
+                class="btn btn-warning item-main start-p">Pause</button>
+            <button 
+                v-else-if="this.isTimerPaused && !this.isTimerReset" 
+                @click="handleResume" 
+                type="button" 
+                class="btn btn-success item-main start-p">Resume</button>
+        </transition>
+        <button 
+            @click="handleAdd" 
+            type="button" 
+            class="btn btn-outline-secondary item-main add-five" 
+            :disabled="!this.isTimerStarted">Add 5'</button>
+        <button 
+            @click="handleReset" 
+            type="button" 
+            class="btn btn-outline-danger item-main stop-p" 
+            :disabled="!this.isTimerStarted">Reset</button>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -20,19 +52,72 @@ export default {
   name: 'MainPomodore',
   data(){
       return {
-          isToggle: false,
-          timerActivate: false,
-          timerStarted: false,
-          timerMinutes: 25,
+          timer: null,
+          remaingTime: 60*25,
+          isTimerReset: true,
+          isTimerStarted: false,
+          isTimerPaused: false,
       }
   },
   computed:{
-  },
+    minutes: function(){
+        const minutes = Math.floor(this.remaingTime/60);
+        if(minutes>10) return minutes
+        else return '0' + minutes;
+    },
+    seconds: function(){
+        const seconds = this.remaingTime - (this.minutes*60);
+        if(seconds>10) return seconds
+        else return '0' + seconds;
+    }, 
+},
   methods:{
+    handleStart: function(){
+        this.timer = setInterval(()=>this.decreaseTimer(),1000);
+        this.isTimerReset = false;
+        this.isTimerStarted=true;
+    },
+    decreaseTimer: function(){
+        if(this.remaingTime>=1){
+            this.remaingTime--;
+        } else{
+            this.remaingTime = 0;
+            this.handlePause();
+        }
+    },
+    handleAdd: function(){
+        this.remaingTime+=60*5;
+    },
+    handlePause: function(){
+        clearInterval(this.timer);
+        this.isTimerPaused = true;
+        this.timer = null;
+    },
+    handleResume: function(){
+        this.handleStart();
+        this.isTimerPaused = false;
+    },
+    handleReset: function(){
+        clearInterval(this.timer);
+        this.remaingTime = 60*25;
+        this.isTimerReset = true;
+        this.isTimerStarted = false;
+        this.isTimerPaused= false;
+        this.timer=null;
+    }
   }
 }
 </script>
 <style>
+.resume{
+    background-color: rgba(0, 255, 34, 0.171) !important;
+    transition: all 0.5s ease-in;
+}
+.pause{
+    background-color: rgba(255, 230, 0, 0.171) !important;
+    transition: all 0.5s ease-in;
+}
+
 .p-title{
     font-size: 0.7em;
 }
@@ -61,4 +146,31 @@ export default {
 .start-p{
     flex: 1 1 100%;
 }
+.bounce-enter-active {
+  animation: bounce-in 0.6s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.6s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.15);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0.1;
+}
+
 </style>
