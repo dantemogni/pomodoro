172900fa13df462b44e9988f818b4e98c6bf7887
Dante Mogni <dantejoaquin.mogni@gmail.com>
2021-06-27 02:20:05 -0300

    minor fixs

--- src/components/MainPomodore.vue
+++ src/components/MainPomodore.vue
@@ -78,25 +78,25 @@
             <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Minutes</span>
             </div>
-        <input v-model="minutesConfig" type="number" id="minutesInput" class="form-control" placeholder="Default: 24" aria-label="minutes" aria-describedby="basic-addon1">
+        <input v-model="minutesConfig" type="number" min="0" id="minutesInput" class="form-control" placeholder="Default: 24" aria-label="minutes" aria-describedby="basic-addon1">
         </div>
             <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Short Minutes</span>
             </div>
-        <input v-model="shortBreakConfig" type="number" id="sessionInput" class="form-control" placeholder="Default: 5" aria-label="sessions" aria-describedby="basic-addon2">
+        <input v-model="shortBreakConfig" type="number" min="0" id="sessionInput" class="form-control" placeholder="Default: 5" aria-label="sessions" aria-describedby="basic-addon2">
         </div>
             <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Long Minutes</span>
             </div>
-        <input v-model="longBreakConfig" type="number" id="sessionInput" class="form-control" placeholder="Default: 15" aria-label="sessions" aria-describedby="basic-addon2">
+        <input v-model="longBreakConfig" type="number" min="0" id="sessionInput" class="form-control" placeholder="Default: 15" aria-label="sessions" aria-describedby="basic-addon2">
         </div>
         <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="basic-addon1">Sessions</span>
             </div>
-        <input v-model="sessionConfig" type="number" id="sessionInput" class="form-control" placeholder="Default: 4" aria-label="sessions" aria-describedby="basic-addon2">
+        <input v-model="sessionConfig" type="number" min="1" id="sessionInput" class="form-control" placeholder="Default: 4" aria-label="sessions" aria-describedby="basic-addon2">
         </div>
         <button 
             @click="this.isSelectedSettings=!this.isSelectedSettings" 
